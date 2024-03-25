595dae35e9ff2e6650c8b274b3acfe6ec8f5a5bf
kicodedev2 <152207718+kicodedev2@users.noreply.github.com>
2024-03-25 13:41:33 +0000

    Automated Code Fix

--- RankingApp/ClientApp/src/components/Item.js
+++ RankingApp/ClientApp/src/components/Item.js
@@ -1,7 +1,8 @@
-﻿const Item = ({item, drag, itemImgObj }) => {
+const Item = ({item, drag, itemImgObj: { image } = {} }) => {
     return (
         <div className="unranked-cell">
-            <img id={`item-${item.id}`} src={itemImgObj.image}
+            <img id={`item-${item.id}`} src={image}
+                alt={`Draggable item ${item.id}`}
                 style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
             />
         </div>     
