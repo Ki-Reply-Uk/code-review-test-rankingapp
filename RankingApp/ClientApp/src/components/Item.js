3e4a430856f5f860c6fe95f7cd15e4823a6dd279
kicodedev2 <152207718+kicodedev2@users.noreply.github.com>
2024-03-26 17:02:17 +0000

    Automated Code Fix

--- RankingApp/ClientApp/src/components/Item.js
+++ RankingApp/ClientApp/src/components/Item.js
@@ -1,9 +1,11 @@
 ﻿import PropTypes from 'prop-types';
 
 const Item = ({ item, drag, itemImgObj }) => {
+    const getImageId = itemId => `item-${itemId}`;
     return (
         <div className="unranked-cell">
-            <img id={`item-${item.id}`} src={itemImgObj.image}
+            <img id={getImageId(item.id)} src={itemImgObj.image}
+                alt={itemImgObj.description || 'Item image'}
                 style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
             />
         </div>     
