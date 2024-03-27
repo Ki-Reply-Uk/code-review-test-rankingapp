f2e977f9fb3a3b20ed8465f128b16a4a79f73a2b
kicodedev2 <152207718+kicodedev2@users.noreply.github.com>
2024-03-27 13:11:58 +0000

    Automated Code Fix

--- RankingApp/ClientApp/src/components/Item.js
+++ RankingApp/ClientApp/src/components/Item.js
@@ -1,10 +1,20 @@
-const Item = ({item, drag, itemImgObj }) => {
+import PropTypes from 'prop-types';
+
+const Item = ({ item, drag, image }) => {
     return (
         <div className="unranked-cell">
-            <img id={`item-${item.id}`} src={itemImgObj.image}
-                style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
-            />
+            <img src={image} alt={item.name} style={{ cursor: "pointer" }} draggable="true" onDragStart={drag} />
         </div>     
     )
 }
+
+Item.propTypes = {
+    item: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired
+    }).isRequired,
+    drag: PropTypes.func.isRequired,
+    image: PropTypes.string.isRequired
+};
+
 export default Item;
