0457953e3e9bf2de39fe74119a4a653f1c52a1ad
kicodedev2 <152207718+kicodedev2@users.noreply.github.com>
2024-03-27 15:14:14 +0000

    Automated Code Fix

--- RankingApp/ClientApp/src/components/Item.js
+++ RankingApp/ClientApp/src/components/Item.js
@@ -1,10 +1,35 @@
-const Item = ({item, drag, itemImgObj }) => {
+import PropTypes from 'prop-types';
+
+const Item = ({ item, drag, itemImgObj }) => {
     return (
         <div className="unranked-cell">
-            <img id={`item-${item.id}`} src={itemImgObj.image}
+            <img src={itemImgObj.image}
+                alt={itemImgObj.description || 'Item image'}
                 style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
+                aria-grabbed="false" // ARIA attribute for accessible drag-and-drop
+                role="button" // ARIA role for interactive drag-and-drop
+                tabIndex="0" // Make the element focusable
             />
         </div>     
     )
 }
+
+Item.propTypes = {
+    item: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired
+    }).isRequired,
+    drag: PropTypes.func.isRequired,
+    itemImgObj: PropTypes.shape({
+        image: PropTypes.string.isRequired,
+        description: PropTypes.string
+    }).isRequired
+};
+
+Item.defaultProps = {
+    itemImgObj: {
+        description: '' // Default empty string for alt text if not provided
+    }
+};
+
 export default Item;
