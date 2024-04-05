b5e5f83eb3331af38663c97674d6cdc6d9816a02
kicodedev2 <152207718+kicodedev2@users.noreply.github.com>
2024-04-05 12:38:49 +0100

    Automated Code Fix

--- RankingApp/ClientApp/src/components/Item.js
+++ RankingApp/ClientApp/src/components/Item.js
@@ -1,10 +1,40 @@
-const Item = ({item, drag, itemImgObj }) => {
+import PropTypes from 'prop-types';
+
+/**
+ * Item component represents a draggable item with an image.
+ * @param {Object} props - Component props.
+ * @param {Object} props.item - Item data.
+ * @param {Function} props.drag - Drag handler function.
+ * @param {Object} props.itemImgObj - Image object for the item.
+ */
+const Item = ({ item, drag, itemImgObj }) => {
+    const { id, imageAlt } = item;
+    const { image } = itemImgObj;
+
     return (
         <div className="unranked-cell">
-            <img id={`item-${item.id}`} src={itemImgObj.image}
-                style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
-            />
+            <img src={image} alt={imageAlt} style={{ cursor: "pointer" }}
+                draggable="true" onDragStart={drag}
+                aria-grabbed="true" aria-label={`Draggable item ${imageAlt}`} />
         </div>     
-    )
-}
+    );
+};
+
+Item.propTypes = {
+    item: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        imageAlt: PropTypes.string
+    }).isRequired,
+    drag: PropTypes.func.isRequired,
+    itemImgObj: PropTypes.shape({
+        image: PropTypes.string.isRequired
+    }).isRequired
+};
+
+Item.defaultProps = {
+    item: {
+        imageAlt: 'Item image'
+    }
+};
+
 export default Item;
